f8a3fb9ac26fc21b0e052faecc54a3c658573e84
Justin Winslow <justin@jwin.me>
2023-07-21 16:38:25 -0500

    Simplify and debounce search

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -3,37 +3,27 @@
 import './style.css';
  
 import { debounce } from 'lodash';
-import { useCallback, useEffect } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useCallback, useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
  
 export default function Search({ route }) {
+  const [search, setSearch] = useState('');
   const router = useRouter();
-  const searchParams = useSearchParams();
-  const createQueryString = useCallback(
-    (name: string, value: string) => {
-      const params = new URLSearchParams(searchParams)
-      params.set(name, value)
- 
-      return params.toString()
-    },
-    [searchParams]
-  );
+  const updateRoute = debounce(function() {
+    router.push(`/${route}?search=${search}`);
+  }, 300);
 
-  // useEffect(() => {
-  //   console.log('useEffect', searchParams.get('search'));
-  // }, [searchParams]);
-
-  function search(event) {
-    router.push(`/${route}?` + createQueryString('search', event.target.value));
-  }
-
-  const input = (
-    <input type="text" name="search" placeholder="Search" onChange={search} />
-  )
+  useEffect(updateRoute, [search]);
 
   return (
     <div className="global-search">
-      <input value={searchParams.get('search') || ''} type="text" name="search" placeholder="Search by title" onChange={search} />
+      <input 
+        value={search} 
+        type="text" 
+        name="search" 
+        placeholder="Search by title" 
+        onChange={(e) => setSearch(e.target.value)}
+      />
     </div>
   )
 }
