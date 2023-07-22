460d4d27e557fde37cdd9c81da682684433a71f5
Justin Winslow <justin@jwin.me>
2023-07-22 18:01:50 -0500

    Query book info

--- src/app/books/[id]/page.tsx
+++ src/app/books/[id]/page.tsx
@@ -2,8 +2,10 @@
 
 import './style.css';
 
+import { useState, useEffect } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import { kebabCase } from 'lodash';
+import getBookInfo from './get-book-info';
 
 import books from '../../../store/books';
 
@@ -11,13 +13,21 @@ export default function Detail() {
   const params = useParams();
   const router = useRouter();
   const book = books.find((b) => kebabCase(`${b.Title}-${b.Author}`) == params.id);
-
-  console.log(book);
+  const [bookInfo, setBookInfo] = useState({});
 
   function back(event) {
     event.preventDefault();
     router.back();
   }
+
+  useEffect(() => {
+    getBookInfo({
+      title: book.Title,
+      author: `${book.Author.split(', ')[1]} ${book.Author.split(', ')[0]}`
+    }).then((res) => {
+      setBookInfo(res);
+    });
+  }, []);
 
   return (
     <section className="view detail">
@@ -28,6 +38,7 @@ export default function Detail() {
         {book.Title}<br />
         <span className="author">by {book.Author}</span>
       </h2>
+      <p>{bookInfo?.description?.value}</p>
     </section>
   )
 }
