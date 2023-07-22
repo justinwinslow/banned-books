6ede7eae2549c1566840f368be0defc7f1e232bc
Justin Winslow <justin@jwin.me>
2023-07-22 18:31:15 -0500

    Add file

---
+++ src/app/books/[id]/get-book-info.ts
@@ -0,0 +1,35 @@
+function replaceSpaces(string) {
+  return string.replace(/ /g, '+');
+}
+
+export default async function getBookInfo({author, title}) {
+  // Let's search for the book based on title and author
+  const searchResponse = await fetch(`https://openlibrary.org/search.json?title=${replaceSpaces(title)}&author=${replaceSpaces(author)}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',  
+    },
+    mode: 'cors'
+  });
+  // Unwrap our search result
+  const searchResult = await searchResponse.json();
+  
+  // Find the path to the book information
+  const id = searchResult?.docs?.[0]?.key;
+
+  // If we didn't find one, let's return 
+  if (!id) return undefined;
+    
+  // If we did, let's query the book information
+  const bookResponse = await fetch(`https://openlibrary.org${id}.json`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',  
+    },
+    mode: 'cors'
+  });
+  // Unrwrap the response
+  const bookInfo = await bookResponse.json();
+
+  return bookInfo;  
+}
