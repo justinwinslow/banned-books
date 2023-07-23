186e8387563bf467a8c7acfc736956eeb2406118
Justin Winslow <justin@jwin.me>
2023-07-23 01:06:00 -0500

    Improve filtering

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -2,6 +2,7 @@
 
 import './style.css';
 
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
 import levenshtein from 'js-levenshtein';
@@ -14,30 +15,34 @@ import fixName from '../../util/fix-name';
 export default function Books() {
   const searchParams = useSearchParams();
   const search = searchParams.get('search');
+  const [filteredBooks, setFilteredBooks] = useState([].concat(books));
 
-  function filteredBooks (books) {
-    return books.filter(b => {
+  useEffect(() => {
+    setFilteredBooks(books.filter(b => {
       /* TODO - Add loose matching */
       return b.Title.toLowerCase().includes((search || '').toLowerCase()); // || levenshtein(b.Title, search) <= 2;
     })
     .sort((a, b) => {
       return a.Title.localeCompare(b.Title);
-    });
+    }));
     /* TODO - Sort based on proximity to search query */
     // .sort((a, b) => {
     //   console.log(levenshtein(a.Title, search), levenshtein(b.Title, search));
     //   return levenshtein(b.Title, search) - levenshtein(a.Title, search);
     // });
-  }
+  }, [search])
+
 
   /*
-    TODO - Save scroll position when navigating to detail views
+    TODO
+    [ ] No Results state
+    [ ] Save scroll position when navigating to detail views
   */
 
   return (
     <section className="view books">
       <ul className="books">
-        {filteredBooks(books).map(b => (
+        {filteredBooks.map(b => (
           <li key={`${b.Title}-${b.Author}`}>
             <Link href={`/books/${encodeURIComponent(kebabCase(`${b.Title}-${b.Author}`))}`}>
               <header>
