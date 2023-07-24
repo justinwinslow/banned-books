896c6b6096956a6e4f47422676b25d5decf46e7d
Justin Winslow <justin@jwin.me>
2023-07-24 17:08:26 -0500

    About content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,6 +9,13 @@ export default function Donate() {
           With all the recent book banning in the US I realized there wasn't an easily searchable index of all the various bans. 
           So, I set out to create this to improve the transparency around our current censorious moment.
         </p>
+        <p>
+          There is a lot more I want to do &ndash; like add better filtering, include more information about bans, 
+          provide resources for activism, and more &ndash; but time is money. Any contribution you make can help me
+          budget more time to this project. If you aren't in a place where you can provide financial support, <a href="https://openlibrary.org/" target="_blank">Open Library</a> is the free resource I use to fill 
+          in information about each book &ndash; as you poke around you may find gaps in the data. If you have time you 
+          can contribute updates through them.
+        </p>
         <h2>Credits</h2>
         <ul>
           <li>Ban data provided by <a href="https://pen.org/banned-in-the-usa/#what" target="_blank">PEN America</a></li>
