1fb60c8a76c8bc8ca326cf4f2ca70a2121992aba
Justin Winslow <justin@jwin.me>
2023-07-24 21:34:43 -0500

    Edit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,16 +6,21 @@ export default function Donate() {
       <div className="info">
         <h2>What is this?</h2>
         <p>
-          With all the recent book banning in the US I realized there wasn't an easily searchable index of all the various bans. 
-          So, I set out to create this to improve the transparency around our current censorious moment.
+          With all the recent book banning in the US, I realized there wasn't an easily searchable index of the various bans. 
+          So, I set out to create this site to improve the transparency around our current censorious moment.
         </p>
         <p>
-          There is a lot more I want to do &ndash; like add better filtering, include more information about bans, 
-          provide resources for activism, and more &ndash; but time is money. Any contribution you make can help me
-          budget more time to this project. If you aren't in a place where you can provide financial support, <a href="https://openlibrary.org/" target="_blank">Open Library</a> is the free resource I use to fill 
-          in information about each book &ndash; as you poke around you may find gaps in the data. If you have time you 
-          can contribute updates through them.
+          There is a lot more I want to do – like add better filtering, include more information about bans, 
+          provide resources for activism, and more – but time is money. 
+          Any contribution you make can help me budget more time to this project. 
         </p>
+        <p>
+          If you aren't in a place where you can provide financial support, you can still help! 
+          Open Library is the free resource I use to fill in information about each book &ndash; 
+          as you poke around the site you may find gaps in the data. 
+          If you have time you can contribute corrections, descriptions, or cover art through them which helps more than just us. 
+        </p>
+
         <h2>Credits</h2>
         <ul>
           <li>Ban data provided by <a href="https://pen.org/banned-in-the-usa/#what" target="_blank">PEN America</a></li>
