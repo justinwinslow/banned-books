78610187b5270def8683b8bfbe4891b2ac31ff86
Justin Winslow <justin@jwin.me>
2023-07-30 18:00:39 -0500

    Move to flex and fix favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import '/node_modules/normalize.css/normalize.css';
 import './globals.css';
 
@@ -7,56 +5,41 @@ import { Shadows_Into_Light } from 'next/font/google';
 import Link from 'next/link';
 import Head from 'next/head';
 import Search from '../components/search';
-
-import { usePathname } from 'next/navigation';
+import { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'School Book Bans',
+  description: 'A search engine for banned books in US schools',
+  icons: {
+    icon: {
+      url: '/icon.png',
+      type: 'image/png',
+    }
+  }
+}
 
 const shadowsIntoLight = Shadows_Into_Light({ weight: '400', subsets: ['latin'] });
 
 export default function RootLayout({
   children,
+  params
 }: {
-  children: React.ReactNode
-}) {
-  // const views = [
-  //   {
-  //     href: '/',
-  //     name: 'Home'
-  //   },
-  //   { 
-  //     href: '/books',
-  //     name: 'Books'
-  //   }
-  // ];
-
-  // Don't show banner search if on the home page
-  const pathname = usePathname();
-  function showGlobalSearch() {
-    if (pathname == '/books') {
-      return (<Search route="books" />);
-    }
+  children: React.ReactNode,
+  params: {
+    page: string,
+    id: string
   }
-
+}) {
   return (
     <html lang="en">
-      <Head>
-        <meta http-equiv="Content-Security-Policy" content="default-src 'self'" />
-        <meta name="theme-color" content="#2a3544" />
-        <meta name="description" content="A search engine for banned books" />
-        <meta name="robots" content="index,follow" />
-        <meta name="googlebot" content="index,follow" />
-        <meta name="subject" content="School book bans" />
-        <meta name="rating" content="General" />
-        <link rel="icon" sizes="192x192" href="/icon.png" />
-        <title>School Book Bans</title>
-      </Head>
       <body>
         <header>
           <Link href="/">
             <h1 className={shadowsIntoLight.className}>School Book Bans</h1>
           </Link>
-          {showGlobalSearch()}
+          <Search route="books" />
         </header>
-        <main className={showGlobalSearch() ? 'with-search' : ''}>
+        <main>
           {children}
         </main>
       </body>
--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -4,18 +4,29 @@ import './style.css';
  
 import { debounce } from 'lodash';
 import { useCallback, useEffect, useState} from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams, usePathname } from 'next/navigation';
  
 export default function Search({ route }: {route: string}) {
   const searchParams = useSearchParams();
   const [search, setSearch] = useState<string>(searchParams.get('search') || '');
   const router = useRouter();
+  const [initialized, setInitialized] = useState<boolean>(false);
   const updateRoute = debounce(function() {
-    router.push(`/${route}?search=${search}`);
+    if (initialized) {
+      router.push(`/${route}?search=${search}`);  
+    } else {
+      // NOTE - This is a bit of a hacky way to get it to not
+      // run on page load
+      setInitialized(true);
+    }
   }, 400);
+  const pathname = usePathname();
 
   useEffect(updateRoute, [search]);
 
+  if (pathname == '/') {
+    return (<></>);
+  }
   return (
     <div className="global-search">
       <input 
