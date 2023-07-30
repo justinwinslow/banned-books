5a040cd2340bc05c3f1118d0ad382213e7a8e0ba
Justin Winslow <justin@jwin.me>
2023-07-30 16:04:12 -0500

    Tweak build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
   distDir: 'dist',
-  trailingSlash: false,
+  // trailingSlash: false,
 }
 
 module.exports = nextConfig
