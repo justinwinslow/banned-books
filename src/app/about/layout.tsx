23ed85d58e1b3d2a98db25ec1e28eaf9b29ad9af
Justin Winslow <justin@jwin.me>
2023-08-08 17:25:53 -0500

    More fun with titles

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: `Search | School Book Bans`
+  title: `About | School Book Bans`
 }
 
 export default function SearchLayout({
