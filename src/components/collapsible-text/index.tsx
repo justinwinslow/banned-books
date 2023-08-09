251163141c9e6c3fd783e0d478def26b8c89260f
Justin Winslow <justin@jwin.me>
2023-08-08 22:38:24 -0500

    Fix logic and some comments

--- src/components/collapsible-text/index.tsx
+++ src/components/collapsible-text/index.tsx
@@ -4,16 +4,23 @@ import { useEffect, useState } from 'react';
 
 export default function CollapsibleText({ content, maxWordCount, defaultContent }: {content: string, maxWordCount: number, defaultContent: any}) {
   defaultContent = defaultContent || 'No text provided';
-  const truncatedText = content.length ? `${content.split(' ').slice(0, maxWordCount).join(' ')} ...` : defaultContent;
-  const [text, setText] = useState(truncatedText);
-  const [expanded, setExpanded] = useState(false);
+  // Limit our initial word count. Default is 100
+  const truncatedText = content.length ? `${content.split(' ').slice(0, maxWordCount).join(' ')}` : defaultContent;
+  // If our provided content is > 100 words, we can expand/collapse it
   const expandable = content.length > truncatedText.length;
-
+  // We'll use "text" as our variable for populating the view
+  const [text, setText] = useState(expandable ? `${truncatedText}...` : content || defaultContent);
+  // And "expanded" will help us key general state side-effects
+  const [expanded, setExpanded] = useState(false);
+  
   function toggle() {
     if (!expanded) {
       setText(content);
     } else {
-      setText(truncatedText);
+      // NOTE - we're adding the ellipsis here which 
+      // makes our initial word count logic not too
+      // annoyiong
+      setText(truncatedText + '...');
     }
 
     setExpanded(!expanded);
